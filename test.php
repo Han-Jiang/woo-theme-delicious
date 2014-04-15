32f895903145035f961acca04fbc830d2431bcf3
Han <1129966399@qq.com>
2014-04-15 11:36:43 +0100

    一些原版的备份文件

---
+++ test.php
@@ -0,0 +1,163 @@
+
+<div data-role="page">
+
+	<div id="container"><div id="content" role="main"><nav class="woocommerce-breadcrumb" itemprop="breadcrumb"><a class="home" href="http://localhost/wordpress">Home</a> &#47; <a href="http://localhost/wordpress/product-category/%e6%b9%98%e8%8f%9c">湘菜</a> &#47; 金龙戏珠</nav>
+		
+			
+
+<div itemscope itemtype="http://schema.org/Product" id="product-485" class="post-485 product type-product status-publish sale shipping-taxable purchasable product-type-simple product-cat-%e6%b9%98%e8%8f%9c instock">
+
+	
+	<span class="onsale">Sale!</span>
+<div class="images">
+
+	<a href="http://localhost/wordpress/wp-content/uploads/2014/04/images-5.jpg" itemprop="image" class="woocommerce-main-image zoom" title="images (5)" data-rel="prettyPhoto"><img width="275" height="183" src="http://localhost/wordpress/wp-content/uploads/2014/04/images-5.jpg" class="attachment-shop_single wp-post-image" alt="images (5)" title="images (5)" /></a>
+	
+</div>
+
+	<div class="summary entry-summary">
+
+		<h1 itemprop="name" class="product_title entry-title">金龙戏珠</h1><div itemprop="offers" itemscope itemtype="http://schema.org/Offer">
+
+	<p class="price"><del><span class="amount">&pound;23.00</span></del> <ins><span class="amount">&pound;20.00</span></ins></p>
+
+	<meta itemprop="price" content="20" />
+	<meta itemprop="priceCurrency" content="GBP" />
+	<link itemprop="availability" href="http://schema.org/InStock" />
+
+</div>
+
+
+	
+	<form class="cart" method="post" enctype='multipart/form-data'>
+	 	
+	 	<div class="quantity"><input type="number" step="1" min="1"  name="quantity" value="1" title="Qty" class="input-text qty text" size="4" /></div>
+	 	<input type="hidden" name="add-to-cart" value="485" />
+
+	 	<button type="submit" class="single_add_to_cart_button button alt">Add to cart</button>
+
+			</form>
+
+	
+<div class="product_meta">
+
+	
+	
+		<span class="sku_wrapper">SKU: <span class="sku" itemprop="sku">2</span>.</span>
+
+	
+	
+	
+	
+</div>
+
+	</div><!-- .summary -->
+
+	
+	<div class="woocommerce-tabs">
+		<ul class="tabs">
+			
+				<li class="reviews_tab">
+					<a href="#tab-reviews">Reviews (0)</a>
+				</li>
+
+					</ul>
+		
+			<div class="panel entry-content" id="tab-reviews">
+				<div id="reviews">
+	<div id="comments">
+		<h2>Reviews</h2>
+
+		
+			<p class="woocommerce-noreviews">There are no reviews yet.</p>
+
+			</div>
+
+	
+		<div id="review_form_wrapper">
+			<div id="review_form">
+												<div id="respond" class="comment-respond">
+				<h3 id="reply-title" class="comment-reply-title">Be the first to review &ldquo;金龙戏珠&rdquo; <small><a rel="nofollow" id="cancel-comment-reply-link" href="/wordpress/?product=%E9%87%91%E9%BE%99%E6%88%8F%E7%8F%A0&amp;_=1397316915395#respond" style="display:none;">Cancel reply</a></small></h3>
+									<form action="http://localhost/wordpress/wp-comments-post.php" method="post" id="commentform" class="comment-form">
+																																						<p class="comment-form-rating"><label for="rating">Your Rating</label><select name="rating" id="rating">
+							<option value="">Rate&hellip;</option>
+							<option value="5">Perfect</option>
+							<option value="4">Good</option>
+							<option value="3">Average</option>
+							<option value="2">Not that bad</option>
+							<option value="1">Very Poor</option>
+						</select></p><p class="comment-form-comment"><label for="comment">Your Review</label><textarea id="comment" name="comment" cols="45" rows="8" aria-required="true"></textarea><input type="hidden" id="_wpnonce" name="_wpnonce" value="92fe2fb8c9" /><input type="hidden" name="_wp_http_referer" value="/wordpress/?product=%E9%87%91%E9%BE%99%E6%88%8F%E7%8F%A0&amp;_=1397316915395" /></p>												<p class="form-submit">
+							<input name="submit" type="submit" id="submit" value="Submit" />
+							<input type='hidden' name='comment_post_ID' value='485' id='comment_post_ID' />
+<input type='hidden' name='comment_parent' id='comment_parent' value='0' />
+						</p>
+						<input type="hidden" id="_wp_unfiltered_html_comment_disabled" name="_wp_unfiltered_html_comment_disabled" value="dde6f687d4" /><script>(function(){if(window===window.parent){document.getElementById('_wp_unfiltered_html_comment_disabled').name='_wp_unfiltered_html_comment';}})();</script>
+					</form>
+							</div><!-- #respond -->
+						</div>
+		</div>
+
+	
+	<div class="clear"></div>
+</div>			</div>
+
+			</div>
+
+
+	<meta itemprop="url" content="http://localhost/wordpress/product/%e9%87%91%e9%be%99%e6%88%8f%e7%8f%a0" />
+
+</div><!-- #product-485 -->
+
+
+		
+	</div></div>
+		<div id="sidebar" role="complementary">
+		<ul>
+						<li>
+				<form role="search" method="get" id="searchform" class="searchform" action="http://localhost/wordpress/">
+				<div>
+					<label class="screen-reader-text" for="s">Search for:</label>
+					<input type="text" value="" name="s" id="s" />
+					<input type="submit" id="searchsubmit" value="Search" />
+				</div>
+			</form>			</li>
+
+			<!-- Author information is disabled per default. Uncomment and fill in your details if you want to use it.
+			<li><h2>Author</h2>
+			<p>A little something about you, the author. Nothing lengthy, just an overview.</p>
+			</li>
+			-->
+
+					</ul>
+		<ul role="navigation">
+			<li class="pagenav"><h2>Pages</h2><ul><li class="page_item page-item-465"><a href="http://localhost/wordpress/cart">Cart</a></li>
+<li class="page_item page-item-466"><a href="http://localhost/wordpress/checkout">Checkout</a></li>
+<li class="page_item page-item-467"><a href="http://localhost/wordpress/my-account">My Account</a></li>
+<li class="page_item page-item-464 current_page_parent"><a href="http://localhost/wordpress/shop">Shop</a></li>
+<li class="page_item page-item-448"><a href="http://localhost/wordpress/448-2">归档</a></li>
+</ul></li>
+			<li><h2>Archives</h2>
+				<ul>
+					<li><a href='http://localhost/wordpress/archives/date/2014/04'>April 2014</a></li>
+	<li><a href='http://localhost/wordpress/archives/date/2014/02'>February 2014</a></li>
+	<li><a href='http://localhost/wordpress/archives/date/2014/01'>January 2014</a></li>
+	<li><a href='http://localhost/wordpress/archives/date/2013/12'>December 2013</a></li>
+				</ul>
+			</li>
+
+			<li class="categories"><h2>Categories</h2><ul>	<li class="cat-item cat-item-4"><a href="http://localhost/wordpress/archives/category/blog" title="View all posts filed under Blog">Blog</a> (6)
+</li>
+	<li class="cat-item cat-item-8"><a href="http://localhost/wordpress/archives/category/english" title="英语相关">english</a> (3)
+</li>
+	<li class="cat-item cat-item-10"><a href="http://localhost/wordpress/archives/category/meta-learning" title="编程元学习
+">meta-learning</a> (26)
+</li>
+	<li class="cat-item cat-item-1"><a href="http://localhost/wordpress/archives/category/uncategorized" title="View all posts filed under Uncategorized">Uncategorized</a> (2)
+</li>
+</ul></li>		</ul>
+		<ul>
+			
+					</ul>
+	</div>
+
+</div>
